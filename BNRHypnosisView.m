6eb23f295f5c1a9b1a0b8bd5bcc08740b3c71389
Steven <steven.github@gmail.com>
2016-05-19 19:09:49 -0700

    initial commit

---
+++ BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Steven Liu on 2016-05-19.
+//  Copyright © 2016 Steven Liu. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Steven Liu on 2016-05-19.
+//  Copyright © 2016 Steven Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
